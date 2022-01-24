afed38c35f3ff908f27e48cd893bdbe5314cfe02
agusgrance <agustingran@gmail.com>
2022-01-23 22:11:22 -0300

    add categories

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
 import Nav from './components/Nav';
 import Card from './components/Card';
 import Slider from './components/Slider';
+import Categories from './components/Categories';
 
 function App() {
   return (
     <div>
-      <Nav/>
+      <Nav/> 
       <Slider img1="./img1.jpg" img2="./img2.jpg" img3="./img3.jpg" />
+      <Categories />
       <Card user="Agustin Grance" title="Titulo..." text="Texto enviado desde una prop"/>
       <Card user="user 1" title="Titulo..." text="Texto enviado desde una prop"/>
       <Card user="user 2" title="Titulo..." text="Texto enviado desde una prop"/>
---
+++ src/components/Categories.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+export default function Categories() {
+  return (
+    <div className="categories">
+      <ul>
+        <li>
+          <i class="fas fa-tree"></i>
+        </li>
+        <li>
+          <i class="fas fa-heart-broken"></i>
+        </li>
+        <li>
+          <i class="fas fa-gamepad"></i>
+        </li>
+        <li>
+          <i class="fas fa-cloud"></i>
+        </li>
+        <li>
+          <i class="fas fa-hat-wizard"></i>
+        </li>
+        <li>
+          <i class="fas fa-headphones"></i>
+        </li>
+      </ul>
+    </div>
+  );
+}
